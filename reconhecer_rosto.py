72234ec53548490c16c9b2781dd5b98f98af799d
Thais <134238333+thais-laura@users.noreply.github.com>
2024-04-27 11:39:50 -0300

    Update reconhecer_rosto.py

--- reconhecer_rosto.py
+++ reconhecer_rosto.py
@@ -5,81 +5,6 @@ cap = cv2.VideoCapture(0) #captura o video da webcam
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml') #classificador já pré treinado que eciste na biblioteca do open cv que identifica rostos
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml') #classificador já pré treinado que eciste na biblioteca do open cv que identifica olhos
 
-# distance from camera to object(face) measured
-# centimeter
-Known_distance = 76.2
- 
-# width of face in the real world or Object Plane
-# centimeter
-Known_width = 14.3
- 
-# Colors
-GREEN = (0, 255, 0)
-RED = (0, 0, 255)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
- 
-# defining the fonts
-fonts = cv2.FONT_HERSHEY_COMPLEX
- 
-# face detector object
-face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-
-def Focal_Length_Finder(measured_distance, real_width, width_in_rf_image):
- 
-    # finding the focal length
-    focal_length = (width_in_rf_image * measured_distance) / real_width
-    return focal_length
- 
-# distance estimation function
-def Distance_finder(Focal_Length, real_face_width, face_width_in_frame):
- 
-    distance = (real_face_width * Focal_Length)/face_width_in_frame
- 
-    # return the distance
-    return distance
- 
- 
-def face_data(image):
- 
-    face_width = 0  # making face width to zero
- 
-    # converting color image ot gray scale image
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
- 
-    # detecting face in the image
-    faces = face_detector.detectMultiScale(gray_image, 1.3, 5)
- 
-    # looping through the faces detect in the image
-    # getting coordinates x, y , width and height
-    for (x, y, h, w) in faces:
- 
-        # draw the rectangle on the face
-        cv2.rectangle(image, (x, y), (x+w, y+h), GREEN, 2)
- 
-        # getting face width in the pixels
-        face_width = w
- 
-    # return the face width in pixel
-    return face_width
- 
- 
-# reading reference_image from directory
-ref_image = cv2.imread("Ref_image.png")
- 
-# find the face width(pixels) in the reference_image
-ref_image_face_width = face_data(ref_image)
- 
-# get the focal by calling "Focal_Length_Finder"
-# face width in reference(pixels),
-# Known_distance(centimeters),
-# known_width(centimeters)
-Focal_length_found = Focal_Length_Finder(
-    Known_distance, Known_width, ref_image_face_width)
- 
-print(Focal_length_found)
-
-
 while True:
     ret, frame = cap.read() #forma que nossa imagem vai ser apresentada(frame)
 
@@ -88,10 +13,7 @@ while True:
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5) #para desenhar um retangulo azul em volta do rosto encontrado
-
-        f = FocalLength(w)
-        Distance_finder(f, w)
-
+        
         #obterá a localizao do rosto identificado com a imagem cinza e colorida
         roi_gray = gray[y:y+w, x:x+w] 
         roi_color = frame[y:y+h, x:x+w]
@@ -107,4 +29,3 @@ while True:
 
 cap.release() #libera o recurso da camera
 cv2.destroyAllWindows() #desativando a webcam
-
